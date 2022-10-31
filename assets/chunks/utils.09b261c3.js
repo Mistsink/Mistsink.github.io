433048768e1438c20e6cf2d91d07d19c6cd4f55b
Mistsink <Mistsink@users.noreply.github.com>
2022-10-31 06:46:36 +0000

    deploy: 3e22a3b86f8b45bf7e63753d566379eadb550ea0

---
+++ assets/chunks/utils.09b261c3.js
@@ -0,0 +1,1 @@
+const c=e=>()=>{let t=document.documentElement.scrollTop||document.body.scrollTop,o=document.documentElement.clientHeight,l=document.documentElement.scrollHeight;t+o>=l&&e()};export{c as u};
